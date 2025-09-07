63e36c272ff87b92fe4f78c346fda92bf69a01bd
Kundan Ghosh <kundan_ghosh@yahoo.com>
2025-09-07 04:06:19 +0100

    placeholder

--- gitmerge.sh
+++ gitmerge.sh
@@ -16,10 +16,10 @@ echo "--- Committing local changes... ---"
 git commit -am "placeholder"
 echo "--- Changes committed successfully. ---"
 
-# 1b. push local changes to remote branch tracking local branch
-echo "-- Pushing local changes to remote local.. ---"
-git push
-echo "--- Changes pushed to remote local.. ---"
+# # 1b. push local changes to remote branch tracking local branch
+# echo "-- Pushing local changes to remote local.. ---"
+# git push
+# echo "--- Changes pushed to remote local.. ---"
 
 # 2. Switch to the local 'master' branch
 echo "--- Switching to the 'master' branch... ---"
